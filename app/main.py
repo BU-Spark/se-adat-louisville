a816d92e09323b44d70fc988fc1d71ab862b2dca
Ramona Bergeron <gberg@bu.edu>
2025-10-30 18:26:08 -0400

    changes for type errors and added small test script for temporary testing post

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+SUPABASE_SCHEMA = os.getenv("SUPABASE_SCHEMA", "public")
 
 if not SUPABASE_URL or not SUPABASE_SERVICE_ROLE_KEY:
     raise RuntimeError("Please set SUPABASE_URL and SUPABASE_SERVICE_ROLE_KEY in environment or .env file")
